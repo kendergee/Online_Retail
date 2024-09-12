42893716ffd374379b89e3dc2565b2666d731671
kendergee <kendergee@Kendergees-Macbook-Air-M1.local>
2024-09-12 15:13:39 +0800

    將range大的數字轉bin的階層聚合

--- customer_classify/new_cluster/cluster.py
+++ customer_classify/new_cluster/cluster.py
@@ -5,19 +5,26 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import fcluster
 from sklearn.metrics import silhouette_score
+from sklearn.decomposition import PCA
 
 datasets = pd.read_csv('/Users/kendergee/Desktop/vscode/online_retail/fake_customer_data.csv')
 datasets = datasets.dropna()
 datasets = datasets[datasets['Quartile'] == 4]
-datasets = datasets.drop(columns=['Quartile', 'Average spending', 'Customer ID','Children','Mariage','Country','Frequency'])
+datasets = datasets.drop(columns=['Quartile', 'Average spending', 'Customer ID'])
+
+datasets['Age'] = pd.cut(datasets['Age'], bins=[0, 30, 40, 50, 60, 70, 80], labels=['0-30', '31-40', '41-50', '51-60', '61-70', '71-80'])
+datasets['Income'] = pd.cut(datasets['Income'], bins=[0, 20000, 40000, 60000, 80000, 100000], labels=['0-20000', '20001-40000', '40001-60000', '60001-80000', '80001-100000'])
+print(datasets.info())
 
 le = LabelEncoder()
-for column in ['Gender', 'Education']:
+for column in ['Age','Gender', 'Education','Country','Mariage','Income']:
     datasets[column] = le.fit_transform(datasets[column])
 
 sc = StandardScaler()
-datasets[['Total spending', 'Age', 'Income','Gender', 'Education']] = sc.fit_transform(datasets[['Total spending', 'Age', 'Income', 'Gender', 'Education']])
+datasets[datasets.columns] = sc.fit_transform(datasets[datasets.columns])
 
+pca = PCA(n_components=0.9)
+datasets = pca.fit_transform(datasets)
 linked = linkage(datasets, method='ward')
 
 plt.figure(figsize=(10, 7))
