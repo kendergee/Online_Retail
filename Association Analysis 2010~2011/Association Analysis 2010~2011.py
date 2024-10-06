120682581320b4098746514fda7301c4e2fd65a9
kendergee <marioass80@gmail.com>
2024-10-06 14:46:07 +0800

    儲存最新的檔案資料

--- Association Analysis 2010~2011/Association Analysis 2010~2011.py
+++ Association Analysis 2010~2011/Association Analysis 2010~2011.py
@@ -9,17 +9,17 @@ from mlxtend.frequent_patterns import apriori,association_rules
 # datap2.to_csv('online_retail_II_2010~2011.csv')
 
 # 建立商品清單2009~2010
-# data = pd.read_csv(r'E:\vscode\Online_Retail\online_retail_II_2009~2010.csv')
+# data = pd.read_csv(r'E:\vscode\Online_Retail\online_retail_II_2010~2011.csv')
 # product_table={}
 # for index,row in data.iterrows():
 #     stock_code = row['StockCode']
 #     description = row['Description']
 #     product_table[stock_code] = description
 # product_table = pd.DataFrame(list(product_table.items()),columns=['StockCode','Description'])
-# product_table.to_csv('Stock and Description 2009~2010.csv')
+# product_table.to_csv('Stock and Description 2010~2011.csv')
 
 # 關聯分析
-# data = pd.read_csv(r'E:\vscode\Online_Retail\online_retail_II_2009~2010.csv')
+# data = pd.read_csv(r'E:\vscode\Online_Retail\online_retail_II_2010~2011.csv')
 # data = data.sort_values('Invoice')
 # data = data[['Invoice','StockCode']]
 # data = data.dropna()
@@ -27,14 +27,14 @@ from mlxtend.frequent_patterns import apriori,association_rules
 # basket = basket.applymap(lambda x:1 if x>0 else 0)
 # frequent_itemsets = apriori(basket,min_support=0.01,use_colnames=True)
 # rules = association_rules(frequent_itemsets,metric='lift',min_threshold=1)
-# rules.to_csv('Association Analysis 2009~2010.csv')
+# rules.to_csv('Association Analysis 2010~2011.csv')
 
 #查看前十名常見的組合
-data = pd.read_csv(r'E:\vscode\Online_Retail\Association Analysis 2009~2010\Association Analysis 2009~2010.csv')
+data = pd.read_csv(r'E:\vscode\Online_Retail\Association Analysis 2010~2011 copy\Association Analysis 2010~2011.csv')
 print(len(data))
 confidence_thresh =0.5
 lift_thresh= 1.5
-support = 0.02
+support = 0.03
 data = data[(data['confidence']>=confidence_thresh) & (data['lift']>=lift_thresh) & (data['support']>=support)]
 data = data.sort_values(by='lift',ascending=False)
 print(data)
---
+++ Quarter trend 2009~2010/Quarter trend 2009~2010 analysis.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pprint import pprint
+import os
+
+data = pd.read_csv(r'E:\vscode\Online_Retail\online_retail_II_2009~2010.csv')
+data['InvoiceDate'] = pd.to_datetime(data['InvoiceDate'])
+data['InvoiceDate'] = data['InvoiceDate'].dt.quarter
+data['SalesAmount'] = data['Quantity']*data['Price']
+
+#全球趨勢分析，繪製每個quarter的銷售筆數和銷售額度的bar plot
+Quarter_SalesAmount = data.groupby('InvoiceDate')['SalesAmount'].sum()
+Quarter_InvoiceCounts = data.drop_duplicates(subset='Invoice').groupby('InvoiceDate')['Invoice'].count()
+
+sns.barplot(x=Quarter_InvoiceCounts.index,y=Quarter_InvoiceCounts.values)
+plt.title('Global Quarter Transaction Counts')
+plt.xlabel('Quarter')
+plt.ylabel('Transactions')
+plt.savefig('2009~2010 Global Quarter Transaction Counts picture')
+plt.show()
+
+sns.barplot(x=Quarter_SalesAmount.index,y=Quarter_SalesAmount.values)
+plt.title('Global Quarter SalesAmount')
+plt.xlabel('Quarter')
+plt.ylabel('SalesAmount')
+plt.savefig('2009~2010 Global Quarter SalesAmount picture')
+plt.show()
+
+#區域分析，繪製每個國家每個quarter的銷售筆數和銷售額度的bar plot
+country = data['Country'].unique()
+country_grouped = data.groupby('Country')
+country_df ={}
+for i in country:
+    country_df[i] = country_grouped.get_group(i)
+    country_df[i] = country_df[i][country_df[i]['SalesAmount']>=0]
+    
+    Quarter_SalesAmount = country_df[i].groupby('InvoiceDate')['SalesAmount'].sum()
+    Quarter_InvoiceCounts = country_df[i].drop_duplicates(subset='Invoice').groupby('InvoiceDate')['Invoice'].count()
+
+    folder_path = os.path.join(r'E:\vscode\Online_Retail\Quarter trend 2009~2010\Country png',f'2009~2010 {i}')
+    os.mkdir(folder_path)
+
+    sns.barplot(x=Quarter_InvoiceCounts.index,y=Quarter_InvoiceCounts.values)
+    plt.title(f'{i} Quarter Transaction Counts')
+    plt.xlabel('Quarter')
+    plt.ylabel('Transactions')
+    img = f'2009~2010 {i} Quarter Transaction Counts.png'
+    savepath = os.path.join(folder_path,img)
+    plt.savefig(savepath,dpi=300)
+    plt.clf()
+
+    sns.barplot(x=Quarter_SalesAmount.index,y=Quarter_SalesAmount.values)
+    plt.title(f'{i} Quarter SalesAmount')
+    plt.xlabel('Quarter')
+    plt.ylabel('SalesAmount')
+    img = f'2009~2010 {i} Quarter SalesAmount.png'
+    savepath = os.path.join(folder_path,img)
+    plt.savefig(savepath,dpi=300)
+    plt.clf()
+
+
